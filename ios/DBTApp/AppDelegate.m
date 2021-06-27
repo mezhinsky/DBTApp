1011e5525762e4545296667ae5b2c523b005e549
Dmitry Mezhinsky <mezhinsky.dmitry@gmail.com>
2021-06-27 09:22:51 +0300

    icon

--- ios/DBTApp/AppDelegate.m
+++ ios/DBTApp/AppDelegate.m
@@ -5,6 +5,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
